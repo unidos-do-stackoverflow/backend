f691b126fb0070a1730f0743040f34d00c36cb57
Isabelar07 <isabelarochas@outlook.com.br>
2021-03-23 18:14:13 -0300

    instalei dotenv, adicionei o app.ts com as configurações do express e do cors e  adicionei o servidor no server.ts

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import app from "./app";
+import { AddressInfo } from "net";
+
+import 'dotenv/config';
+
+const server = app.listen(process.env.PORT || 3003, () => {
+	if (server) {
+		const address = server.address() as AddressInfo;
+		console.log(`Server is running in http://localhost: ${address.port}`);
+	} else {
+		console.error(`Failure upon starting server.`);
+	}
+});
+
