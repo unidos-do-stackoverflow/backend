0900117ce93a27ceeddefc7aa808e67b376fe29d
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-03-24 09:38:24 -0300

    build: atualizar config. do linter
    - adicionar convenção para criação de interfaces
    - adicionar regra para ignorar variáveis não utilizadas quando seguirem o padrão '_'
    - definir ambiente do linter (es6, node, jest)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,31 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	plugins: [
-		'@typescript-eslint',
-	],
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-	],
+	plugins: ['@typescript-eslint'],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	rules: {
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{
+				argsIgnorePattern: '_',
+			},
+		],
+
+		'@typescript-eslint/naming-convention': [
+			'error',
+			{
+				selector: 'interface',
+				format: ['PascalCase'],
+				custom: {
+					regex: '^I[A-Z]',
+					match: true,
+				},
+			},
+		],
+	},
+	env: {
+		es6: true,
+		node: true,
+		jest: true,
+	},
 };
