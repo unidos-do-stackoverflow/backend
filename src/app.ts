6b8d6955933df880e1e21884b7c251231516fe16
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-03-24 10:00:08 -0300

    feat: importar rotas no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express, {Express} from 'express';
+import express from 'express';
 import cors from 'cors';
+import routes from './shared/infra/http/routes';
 
-const app: Express = express();
-
-console.log("deu");
+const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(routes);
 
-export default app
+export default app;
