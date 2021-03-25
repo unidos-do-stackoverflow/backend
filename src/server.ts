d7b308bcc1f5c1236f98a4cba8fb9862ee5d457b
Isabelar07 <isabelarochas@outlook.com.br>
2021-03-25 15:42:34 -0300

    fix: remove espaço a mais no link do localhost

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { AddressInfo } from 'net';
 const server = app.listen(process.env.PORT || 3003, () => {
 	if (server) {
 		const address = server.address() as AddressInfo;
-		console.log(`Server is running in http://localhost: ${address.port}`);
+		console.log(`Server is running in http://localhost:${address.port}`);
 	} else {
 		console.error(`Failure upon starting server.`);
 	}
