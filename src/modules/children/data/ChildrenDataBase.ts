e97f519330111e89564b7338582e788d4855be02
Isabelar07 <isabelarochas@outlook.com.br>
2021-04-03 20:24:53 -0300

    feat: inserir crianças no banco de dados

---
+++ src/modules/children/data/ChildrenDataBase.ts
@@ -0,0 +1,31 @@
+import { DataBase } from '../../../shared/infra/data/DataBase';
+import { BaseError } from '../../../shared/infra/error/BaseError';
+import { IChildren } from '../interfaces/IChildren';
+
+export class ChildrenDataBase extends DataBase {
+
+	private static tableName = 'Children';
+
+	public async create(
+		children: IChildren
+	): Promise<void> {
+		try {
+
+			await DataBase.connection()
+			.insert({
+				id: children.id,
+				name: children.name,
+				dateOfBirth: children.dateOfBirth,
+				gender: children.gender,
+				address: children.address,
+				school: children.school,
+				year: children.year,
+				user_id: children.user_id
+			})
+			.into(ChildrenDataBase.tableName);
+
+		} catch (error) {
+			throw new BaseError(error.message || error.sqlMessage);
+		}
+	}
+}
